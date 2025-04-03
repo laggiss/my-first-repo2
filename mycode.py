f4e474442ac7a8537668955fabd87fe5c8eea476
mike.sawada <msawada@uottawa.ca>
2025-04-02 20:07:23 -0400

    added substantial changes to mycode that creates a list of feature classes in a geodatabase workspace

--- mycode.py
+++ mycode.py
@@ -1,4 +1,4 @@
 import arcpy
 from arcpy import env
-env.workspace=r"(C:\Users\msawada\Downloads\Lecture7_Data\Module7_Data\OTTAWA.gdb\Ottawadata)"
-print(arcpy.ListFeatureClasses())
+env.workspace=r"(C:\Users\msawada\Downloads\Lecture7_Data\Module7_Data\OTTAWA.gdb\OTTAWADATA)"
+featureclass_list=arcpy.ListFeatureClasses()
