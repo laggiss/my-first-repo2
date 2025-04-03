8edb78c51fd02d6e0e3ee261de4a8ff388a11e2d
mike.sawada <msawada@uottawa.ca>
2025-04-02 20:29:52 -0400

    made changes in main module for testing

--- mycode.py
+++ mycode.py
@@ -3,5 +3,6 @@ from arcpy import env
 import my_function_module
 env.workspace=r"(C:\Users\msawada\Downloads\Lecture7_Data\Module7_Data\OTTAWA.gdb\OTTAWADATA)"
 featureclass_list=arcpy.ListFeatureClasses()
+fieldnumbervector=[]
 for fc in featureclass_list:
-    print(my_function_module.get_number_fields(fc))
+     fieldnumbervector.append(my_function_module.get_number_fields(fc))
